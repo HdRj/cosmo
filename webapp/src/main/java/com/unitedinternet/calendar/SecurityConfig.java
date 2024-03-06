5c7abb8f3b52db9780bce0c6c9e507c4ca0bc0f2
Yuri <yuriiol@proton.me>
2024-03-06 21:52:24 +0200

    added spring http-security and spring actuator

--- webapp/src/main/java/com/unitedinternet/calendar/SecurityConfig.java
+++ webapp/src/main/java/com/unitedinternet/calendar/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.authentication.BindAuthenticator;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
@@ -11,9 +17,12 @@ import org.springframework.security.ldap.authentication.LdapAuthenticator;
 import org.springframework.security.ldap.server.UnboundIdContainer;
 import org.springframework.security.ldap.userdetails.PersonContextMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.unitedinternet.cosmo.model.EntityFactory;
+import org.unitedinternet.cosmo.service.UserService;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${ldap.port}")
     private String ldapPort;
@@ -22,6 +31,13 @@ public class SecurityConfig {
     @Value("${ldap.parameters}")
     private String ldapParameters;
 
+    private final UserService userService;
+    private final EntityFactory entityFactory;
+
+    public SecurityConfig(UserService userService, EntityFactory entityFactory) {
+        this.userService = userService;
+        this.entityFactory = entityFactory;
+    }
 
     @Bean
     UnboundIdContainer unboundIdContainer() {
@@ -57,6 +73,27 @@ public class SecurityConfig {
         LdapAuthenticationProvider ldapAuthenticationProvider = new LdapAuthenticationProvider(ldapAuthenticator);
         ldapAuthenticationProvider.setUserDetailsContextMapper(new PersonContextMapper());
         return ldapAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/url/**").permitAll()
+                //.antMatchers("/actuator/**").hasRole("USER")
+                .antMatchers("/test/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public AllowAllAuthenticationProvider customAuthenticationProvider() {
+        return new AllowAllAuthenticationProvider(userService,entityFactory);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(customAuthenticationProvider());
     }
 
 }
